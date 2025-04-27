20998f366e3eb9fe2576a800086c676416cde4b5
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-27 20:33:54 -0300

    fix variables clear all

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ ahorcado = Ahorcado()
 
 @app.route("/initPlayers")
 def init():
+    ahorcado.limpiarVariables()
     ahorcado.limpiarUsuarios()
     ahorcado.setUsuarios([Usuario("juan2",".",200),Usuario("fede3",".",100),Usuario("diego1",".",300),Usuario("pedro7",".",150),Usuario("julian9",".",50),
                                 Usuario("damian4",".",250),Usuario("victor11",".",350),Usuario("roberto10",".",25),Usuario("david14",".",125),
