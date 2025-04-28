227161b3379d2ff4bd1f3093e40d66462d9852a5
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-28 11:31:56 -0300

    fix 59

--- frontend/features/login.steps.js
+++ frontend/features/login.steps.js
@@ -3,76 +3,64 @@ const feature = loadFeature("./features/login.feature");
 const { getWebdriver, By } = require("./webdriver");
 jest.setTimeout(10000);
 
-defineFeature(feature, (test) => {
-  let driver;
-
-  beforeEach(async () => {
-    driver = await getWebdriver();
-  });
-
-  afterEach(async () => {
-    if (driver) {
-      await driver.quit();
-    }
-  });
-
+defineFeature(feature, async (test) => {
   test("Login successful", ({ given, when, then }) => {
+    const driver = getWebdriver();
     given("I set franco as username", async () => {
-      await driver.wait(async () => {
+      await driver.wait(function () {
         return driver
           .findElements(By.id("username"))
           .then((found) => !!found.length);
       }, 5000);
       const input = await driver.findElement(By.css("#username input"));
-      await input.sendKeys("franco");
+      input.sendKeys("franco");
     });
-
     given("12345 as password", async () => {
       const input = await driver.findElement(By.css("#password input"));
-      await input.sendKeys("12345");
+      input.sendKeys("12345");
     });
-
     when("I click login", async () => {
       const button = await driver.findElement(By.css("#loginBtn"));
-      await button.click();
+      button.click();
     });
-
     then("I should see Difficulty Selection Page", async () => {
-      await driver.wait(async () => {
+      await driver.wait(function () {
         return driver
           .findElements(By.id("difSelector"))
           .then((found) => !!found.length);
       }, 5000);
+      await new Promise((r) => setTimeout(r, 2000));
+      await driver.close();
     });
   });
 
-  test("Login unsuccessful", ({ given, when, then }) => {
+  test("Login unsuccessful", async ({ given, when, then }) => {
+    const driver = await getWebdriver();
     given("I set franco as username", async () => {
-      await driver.wait(async () => {
+      await driver.wait(function () {
         return driver
           .findElements(By.id("username"))
           .then((found) => !!found.length);
       }, 5000);
       const input = await driver.findElement(By.css("#username input"));
-      await input.sendKeys("franco");
+      input.sendKeys("franco");
     });
-
     given("asdasdasd123123123 as password", async () => {
       const input = await driver.findElement(By.css("#password input"));
-      await input.sendKeys("asdasdasd123123123");
+      input.sendKeys("asdasdasd123123123");
     });
-
     when("I click login", async () => {
       const button = await driver.findElement(By.css("#loginBtn"));
-      await button.click();
+      button.click();
     });
-
     then("I should see a message Invalid username or password", async () => {
-      await driver.wait(async () => {
+      await driver.wait(function () {
         return driver
           .findElements(By.id("loginError"))
           .then((found) => !!found.length);
       }, 5000);
+      await new Promise((r) => setTimeout(r, 2000));
+      await driver.close();
     });
   });
 });
