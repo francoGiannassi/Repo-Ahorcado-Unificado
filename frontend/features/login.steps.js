9ecd3d5819913e8a4c40716f6696f7bdcf3a14da
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-28 14:20:02 -0300

    fix 92

--- frontend/features/login.steps.js
+++ frontend/features/login.steps.js
@@ -9,7 +9,7 @@ defineFeature(feature, (test) => {
 
   beforeEach(async () => {
     driver = await getWebdriver();
-    await driver.get("https://repo-ahorcado-unificado.onrender.com/");
+    await driver.get("http://localhost:8080");
   });
 
   afterEach(async () => {
