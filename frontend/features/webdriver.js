e6cf90876d55aedcef3013357d7e5a7cf5184729
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-28 03:01:34 -0300

    fix 18

--- frontend/features/webdriver.js
+++ frontend/features/webdriver.js
@@ -10,10 +10,12 @@ const getDriver = async () => {
   options.addArguments("--disable-dev-shm-usage");
   options.addArguments("--disable-gpu");
   options.addArguments("--window-size=1920,1080");
-
+  
+  const service = new chrome.ServiceBuilder("/usr/local/bin/chromedriver");
   return await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
+    .setChromeService(service)
     .build();
 };
 
