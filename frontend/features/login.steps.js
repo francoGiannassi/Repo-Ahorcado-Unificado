9014d349e6c35a20cb2a453b150f07a6e6de26f9
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-28 13:58:14 -0300

    fix 88

--- frontend/features/login.steps.js
+++ frontend/features/login.steps.js
@@ -20,93 +20,61 @@ defineFeature(feature, (test) => {
 
   test("Login successful", ({ given, and, when, then }) => {
     given("I set franco as username", async () => {
-      try {
-        await driver.wait(async () => {
-          return driver
-            .findElements(By.id("username"))
-            .then((found) => !!found.length);
-        }, 5000);
-        const input = await driver.findElement(By.css("#username input"));
-        await input.sendKeys("franco");
-      } catch (error) {
-        //console.error("I set franco as username:", error);
-      }
+      await driver.wait(async () => {
+        return driver
+          .findElements(By.id("username"))
+          .then((found) => !!found.length);
+      }, 5000);
+      const input = await driver.findElement(By.css("#username input"));
+      await input.sendKeys("franco");
     });
 
     and("12345 as password", async () => {
-      try {
-        const input = await driver.findElement(By.css("#password input"));
-        await input.sendKeys("12345");
-      } catch (error) {
-        //console.error("12345 as password:", error);
-      }
+      const input = await driver.findElement(By.css("#password input"));
+      await input.sendKeys("12345");
     });
 
     when("I click login", async () => {
-      try {
-        const button = await driver.findElement(By.css("#loginBtn"));
-        await button.click();
-      } catch (error) {
-        //console.error("I click login:", error);
-      }
+      const button = await driver.findElement(By.css("#loginBtn"));
+      await button.click();
     });
 
     then("I should see Difficulty Selection Page", async () => {
-      try {
-        await driver.wait(async () => {
-          return driver
-            .findElements(By.id("difSelector"))
-            .then((found) => !!found.length);
-        }, 5000);
-      } catch (error) {
-        //console.error("I should see Difficulty Selection Page:", error);
-      }
+      await driver.wait(async () => {
+        return driver
+          .findElements(By.id("difSelector"))
+          .then((found) => !!found.length);
+      }, 5000);
     });
   });
 
   test("Login unsuccessful", ({ given, and, when, then }) => {
     given("I set franco as username", async () => {
-      try{
-        await driver.wait(async () => {
-          return driver
-            .findElements(By.id("username"))
-            .then((found) => !!found.length);
-        }, 5000);
-        const input = await driver.findElement(By.css("#username input"));
-        await input.sendKeys("franco");
-      } catch (error) {
-        //console.error("I set franco as username:", error);
-      }
+      await driver.wait(async () => {
+        return driver
+          .findElements(By.id("username"))
+          .then((found) => !!found.length);
+      }, 5000);
+      const input = await driver.findElement(By.css("#username input"));
+      await input.sendKeys("franco");
     });
 
     and("asdasdasd123123123 as password", async () => {
-      try{
-        const input = await driver.findElement(By.css("#password input"));
-        await input.sendKeys("asdasdasd123123123");
-      } catch (error) {
-        //console.error("asdasdasd123123123 as password:", error);
-      }
+      const input = await driver.findElement(By.css("#password input"));
+      await input.sendKeys("asdasdasd123123123");
     });
 
     when("I click login", async () => {
-      try{
-        const button = await driver.findElement(By.css("#loginBtn"));
-        await button.click();
-      } catch (error) {
-        //console.error("I click login:", error);
-      }
+      const button = await driver.findElement(By.css("#loginBtn"));
+      await button.click();
     });
 
     then("I should see a message Invalid username or password", async () => {
-      try{
-        await driver.wait(async () => {
-          return driver
-            .findElements(By.id("loginError"))
-            .then((found) => !!found.length);
-        }, 5000);
-      } catch (error) {
-        //console.error("I should see a message Invalid username or password:", error);
-      }
+      await driver.wait(async () => {
+        return driver
+          .findElements(By.id("loginError"))
+          .then((found) => !!found.length);
+      }, 5000);
     });
   });
 });
