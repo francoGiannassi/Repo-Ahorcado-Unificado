6b82828a58285fc401fc67f332754e6c489129cd
Franco Giannassi <franco.giannassi@gmail.com>
2025-04-27 21:44:01 -0300

    chrome

--- frontend/features/webdriver.js
+++ frontend/features/webdriver.js
@@ -1,6 +1,20 @@
 const { Builder, By } = require("selenium-webdriver");
-const getDriver = async () => await new Builder().forBrowser("chrome").build();
-const url = "http://localhost:8080/home";
+const url = process.env.VUE_APP_AT_URL;
+
+const getDriver = async () => {
+  const chrome = require("selenium-webdriver/chrome");
+  const options = new chrome.Options();
+  options.addArguments("--headless");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--window-size=1920,1080");
+
+  return await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
+};
 
 const getWebdriver = async () => {
   const driver = await getDriver();
